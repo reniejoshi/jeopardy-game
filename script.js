d7f0983b27e47b1f05cb1e7fd8b65cfaeb058d27
reniejoshi <renie_joshi@yahoo.com>
2025-11-22 20:37:35 -0800

    Store fetched question in an object

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const questionElement = document.getElementById("question");
-const categoryElement = document.getElementById("category");
+let question = {};
 
-async function question() {
+async function fetchQuestion() {
     const response = await fetch('https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=multiple');
     const data = await response.json();
     const result = data.results[0];
-    questionElement.textContent = result.question;
-    categoryElement.textContent = result.category;
+
+    question = {
+        type: result.type,
+        difficulty: result.difficulty,
+        category: result.category,
+        question: result.question,
+        correctAnswer: result.correct_answer,
+        incorrectAnswers: result.incorrect_answers
+    };
 }
 
-question();
+fetchQuestion();
