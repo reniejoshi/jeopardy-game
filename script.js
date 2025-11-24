f12fb70a59c9c192c4b555f7123212bad5dece44
reniejoshi <renie_joshi@yahoo.com>
2025-11-23 16:15:23 -0800

    Add try-catch block for error handling

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let questions = [];
 
 async function fetchQuestions() {
-    const response = await fetch(`https://opentdb.com/api.php?amount=3&category=${generateRandomCategory()}&difficulty=easy&type=multiple`);
-    const data = await response.json();
+    try {
+        const response = await fetch(`https://opentdb.com/api.php?amount=3&category=${generateRandomCategory()}&difficulty=easy&type=multiple`);
+        const data = await response.json();
 
-    questions = data.results.map(result => ({
-        type: result.type,
-        difficulty: result.difficulty,
-        category: result.category,
-        question: result.question,
-        correctAnswer: result.correct_answer,
-        incorrectAnswers: result.incorrect_answers
-    }));
+        questions = data.results.map(result => ({
+            type: result.type,
+            difficulty: result.difficulty,
+            category: result.category,
+            question: result.question,
+            correctAnswer: result.correct_answer,
+            incorrectAnswers: result.incorrect_answers
+        }));
 
-    displayQuestions();
+        displayQuestions();
+    } catch (error) {
+        console.error("Error in fetchQuestions():", error);
+    }
 }
 
 function displayQuestions() {
